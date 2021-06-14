2e02725f3768caab95e7c72c100d15c36ec503ef
Rodrigo <rodrigomxsantos@gmail.com>
2021-06-14 01:27:41 -0300

    Primeira versão frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+
+registerLocaleData(localePt);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    SharedModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue:'pt-BR'}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,12 @@
+class Stock {
+  id: number;
+  name: String;
+  price: number;
+  minimum: number;
+  exclusiveMinimum: boolean;
+  date: String;
+  variation: number;
+
+}
+
+export default Stock;
